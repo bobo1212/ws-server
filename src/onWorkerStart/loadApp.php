5d3192e921df96cdaa3cedda981db8ee1771e98b
bobo1212 <bobo1212@wp.pl>
2023-04-27 17:20:49 +0200

    Autoload

--- src/onWorkerStart/loadApp.php
+++ src/onWorkerStart/loadApp.php
@@ -1,19 +1,16 @@
 <?php
-require_once SERVER_DIR . '/vendor/autoload.php';
-require_once SERVER_DIR . '/src/AppInterface.php';
-require_once SERVER_DIR . '/src/app/AppAdmin.php';
-require_once SERVER_DIR . '/src/app/AppToAll.php';
-require_once SERVER_DIR . '/src/app/AppClientServer.php';
-require_once SERVER_DIR . '/src/app/PubSub/AppPubSub.php';
-require_once SERVER_DIR . '/src/app/PubSub/Repo/InterfaceTopic.php';
-require_once SERVER_DIR . '/src/app/PubSub/Repo/RepoTopic.php';
-require_once SERVER_DIR . '/src/app/PubSub/Repo/InterfaceProducer.php';
-require_once SERVER_DIR . '/src/app/PubSub/Repo/RepoProducer.php';
 
+use Bobo1212\WsServerOpenSwoole\app\AppAdmin;
+use Bobo1212\WsServerOpenSwoole\app\AppClientServer;
+use Bobo1212\WsServerOpenSwoole\app\AppToAll;
+use Bobo1212\WsServerOpenSwoole\app\PubSub\AppPubSub;
+
+require_once SERVER_DIR . '/vendor/autoload.php';
 global $appConfig;
+
 $appConfig = [
-    '/admin' => new  \Bobo1212\WsServerOpenSwoole\app\AppAdmin(),
-    '/toall' => new  \Bobo1212\WsServerOpenSwoole\app\AppToAll(),
-    '/ai' => new \Bobo1212\WsServerOpenSwoole\app\AppClientServer('unikalnaNazwa_jsdfh7234'),
-    '/topics' => new \Bobo1212\WsServerOpenSwoole\app\PubSub\AppPubSub(),
+    '/admin' => new  AppAdmin(),
+    '/toall' => new  AppToAll(),
+    '/ai' => new AppClientServer('unikalnaNazwa_jsdfh7234'),
+    '/topics' => new AppPubSub(),
 ];
