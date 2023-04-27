c312ce4206b8f2abc85923bf4c84d68152301458
bobo1212 <bobo1212@wp.pl>
2023-04-27 16:26:59 +0200

    On close, on open

--- src/onWorkerStart/on/onClose.php
+++ src/onWorkerStart/on/onClose.php
@@ -8,8 +8,7 @@ function onClose(Server $server, int $fd)
     global $appConfig;
     $requestUri = getUri($fd);
     $appConfig[$requestUri]->onClose($server, $fd);
-    $requetUri = getUri($fd);
     removeFromTable($fd);
-    $users = getUsers($requetUri);
-    logMsg(LogLevel::INFO, 'connection close: ' . $fd . ' ' . $requetUri . ' ' . count($users));
+    $users = getUsers($requestUri);
+    logMsg(LogLevel::INFO, 'connection close: ' . $fd . ' ' . $requestUri . ' ' . count($users));
 }
--- src/onWorkerStart/on/onOpen.php
+++ src/onWorkerStart/on/onOpen.php
@@ -1,20 +1,20 @@
 <?php
+
 use OpenSwoole\WebSocket\Server;
 use OpenSwoole\Http\Request;
 
-function onOpen(Server $server, Request $request){
+function onOpen(Server $server, Request $request)
+{
     global $appConfig;
-    if(!array_key_exists($request->server['request_uri'],$appConfig)) {
-        $server->disconnect($request->fd, Server::WEBSOCKET_CLOSE_NORMAL,'404');
+    if (!array_key_exists($request->server['request_uri'], $appConfig)) {
+        $server->disconnect($request->fd, Server::WEBSOCKET_CLOSE_NORMAL, '404');
         return;
     }
     setUser($request->server['request_uri'], $request->fd);
     setUri($request->fd, $request->server['request_uri']);
+
     $users = getUsers($request->server['request_uri']);
     logMsg(LogLevel::INFO, 'connection open: ' . $request->fd . ' ' . $request->server['request_uri'] . ' ' . count($users));
 
-
-    $requestUri = getUri($request->fd);
-    $appConfig[$requestUri]->onOpen($server, $request);
-
+    $appConfig[$request->server['request_uri']]->onOpen($server, $request);
 }
